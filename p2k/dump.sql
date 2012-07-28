f60dc1fff5e3546840ecef6b58483785c18031c6
webgeist <webgeist.xor@gmail.com>
2012-07-28 19:01:31 +0400

    база

--- p2k/dump.sql
+++ p2k/dump.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.1.62, for debian-linux-gnu (i486)
+-- MySQL dump 10.13  Distrib 5.1.63, for debian-linux-gnu (i486)
 --
 -- Host: localhost    Database: p2k
 -- ------------------------------------------------------
--- Server version	5.1.62-1~dotdeb.0
+-- Server version	5.1.63-0+squeeze1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -27,8 +27,11 @@ CREATE TABLE `matches` (
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `home` varchar(50) NOT NULL,
   `away` varchar(50) NOT NULL,
+  `home_score` int(11) NOT NULL DEFAULT '0',
+  `away_score` int(11) NOT NULL DEFAULT '0',
+  `get_result` tinyint(4) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=25 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -37,7 +40,7 @@ CREATE TABLE `matches` (
 
 LOCK TABLES `matches` WRITE;
 /*!40000 ALTER TABLE `matches` DISABLE KEYS */;
-INSERT INTO `matches` VALUES (1,'0000-00-00 00:00:00','Польша','Греция'),(2,'2012-06-07 23:19:18','Россия','Чехия');
+INSERT INTO `matches` VALUES (1,'2012-06-09 18:06:05','Польша','Греция',1,1,1),(2,'2012-06-09 18:06:35','Россия','Чехия',4,1,1),(3,'2012-06-09 16:00:00','Нидерланды','Дания',0,1,1),(4,'2012-06-09 18:45:00','Германия','Португалия',1,0,1),(5,'2012-06-10 16:00:00','Испания','Италия',1,1,1),(6,'2012-06-10 18:45:00','Ирландия','Хорватия',0,0,0),(7,'2012-06-11 16:00:00','Франция','Англия',0,0,0),(8,'2012-06-11 18:45:00','Украина','Швеция',0,0,0),(9,'2012-06-12 16:00:00','Греция','Чехия',0,0,0),(10,'2012-06-12 18:45:00','Польша','Россия',0,0,0),(11,'2012-06-13 16:00:00','Дания','Португалия',0,0,0),(12,'2012-06-13 18:45:00','Нидерланды','Германия',0,0,0),(13,'2012-06-14 16:00:40','Италия','Хорватия',0,0,0),(14,'2012-06-14 18:45:10','Испания','Ирландия',0,0,0),(15,'2012-06-15 16:00:00','Украина','Франция',0,0,0),(16,'2012-06-15 18:45:00','Швеция','Англия',0,0,0),(17,'2012-06-16 18:45:10','Греция','Россия',0,0,0),(18,'2012-06-16 18:45:10','Чехия','Польша',0,0,0),(19,'2012-06-17 18:45:10','Португалия','Нидерланды',0,0,0),(20,'2012-06-17 18:45:10','Дания','Германия',0,0,0),(21,'2012-06-18 18:45:10','Хорватия','Испания',0,0,0),(22,'2012-06-18 18:45:10','Италия','Ирландия',0,0,0),(23,'2012-06-19 18:45:10','Швеция','Франция',0,0,0),(24,'2012-06-19 18:45:10','Англия','Украина',0,0,0);
 /*!40000 ALTER TABLE `matches` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -56,7 +59,7 @@ CREATE TABLE `user_matches` (
   `result` tinyint(4) NOT NULL DEFAULT '0',
   `is_done` tinyint(4) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -65,7 +68,7 @@ CREATE TABLE `user_matches` (
 
 LOCK TABLES `user_matches` WRITE;
 /*!40000 ALTER TABLE `user_matches` DISABLE KEYS */;
-INSERT INTO `user_matches` VALUES (1,1,1,1,0,1);
+INSERT INTO `user_matches` VALUES (1,1,1,1,0,1),(2,2,2,1,0,1);
 /*!40000 ALTER TABLE `user_matches` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -102,4 +105,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2012-06-08  8:41:46
+-- Dump completed on 2012-07-28 14:29:40
